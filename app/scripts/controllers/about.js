a4395602d67bea4582df1486ab1728f5a019c01f
Ionut Moraru <morion4000@gmail.com>
2016-08-19 19:43:53 +0300

    Initial commit. Yeoman generated

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hosteroApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hosteroApp
+ */
+angular.module('hosteroApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hosteroApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hosteroApp
+ */
+angular.module('hosteroApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
