f44e4fa51fbde80040c4467b9d241c13162fd88e
Ionut Moraru <morion4000@gmail.com>
2016-10-14 11:12:29 +0300

    Removed scripts

--- scripts/copy_files_to_app_source_dir.sh
+++
@@ -1,6 +0,0 @@
-cd $WERCKER_SOURCE_DIR
-tar cvf code.tar * > /dev/null
-mv code.tar /usr/src/app
-cd /usr/src/app
-tar xvf code.tar > /dev/null
-unlink code.tar
--- scripts/copy_files_to_wercker_output_dir.sh
+++
@@ -1,5 +0,0 @@
-tar cvf code.tar --exclude="_builds/*" --exclude="_projects/*" --exclude="_steps/*" --exclude=".git/*" * > /dev/null
-mv code.tar $WERCKER_OUTPUT_DIR
-cd $WERCKER_OUTPUT_DIR
-tar xvf code.tar > /dev/null
-unlink code.tar
