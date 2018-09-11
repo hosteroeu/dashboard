0815ba053fde3b85a10b00005a219ef44d5b9f04
Ionut Moraru <morion4000@gmail.com>
2018-09-11 16:48:20 +0300

    Updates

--- app/scripts/controllers/root.js
+++ app/scripts/controllers/root.js
@@ -8,8 +8,13 @@
  * Controller of the atlasApp
  */
 angular.module('atlasApp')
-  .controller('RootCtrl', function ($scope) {
+  .controller('RootCtrl', function($scope, $mdDialog) {
     var profile = JSON.parse(localStorage.getItem('profile'));
 
     $scope.profile = profile;
+
+    this.openMenu = function($mdMenu, ev) {
+      console.log('a');
+      $mdMenu.open(ev);
+    };
   });
