b2dbd7a7792cc1cd6f48aa1d9e90667e6e740992
Ionut Moraru <morion4000@gmail.com>
2018-09-11 13:10:37 +0300

    Fixed an issue with the account

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -9,15 +9,25 @@
  */
 angular.module('atlasApp')
   .controller('DashboardCtrl', function ($scope, accountsService) {
-    var account = JSON.parse(localStorage.getItem('account'));
     var profile = JSON.parse(localStorage.getItem('profile'));
+    var account = {
+      name: ''
+    };
 
-    // TODO: Temporary to get the data I didn't get at registration
-    accountsService.update({
-      id: account.id
-    }, {
-      email: profile.email,
-      full_name: profile.name
+    accountsService.get({
+      controller: 'sync'
+    }).$promise.then(function(_account) {
+      account = _account;
+
+      localStorage.setItem('account', JSON.stringify(account));
+
+      // TODO: Temporary to get the data I didn't get at registration
+      accountsService.update({
+        id: account.id
+      }, {
+        email: profile.email,
+        full_name: profile.name
+      });
     });
 
     $scope.getIframeSrc = function(panelId) {
