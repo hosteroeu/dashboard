4f7b6a82d6515c2881f4edd334accdd198f76708
Ionut Moraru <morion4000@gmail.com>
2018-10-03 20:45:27 +0300

    Account ID

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -10,4 +10,5 @@
 angular.module('atlasApp')
   .controller('AccountCtrl', function ($scope) {
     $scope.profile = JSON.parse(localStorage.getItem('profile'));
+    $scope.account = JSON.parse(localStorage.getItem('account'));
   });
