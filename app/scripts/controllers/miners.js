e75c1e44c4178fc29da071da4662f97418b0583e
Ionut Moraru <morion4000@gmail.com>
2018-10-19 16:18:07 +0300

    new miners page

--- app/scripts/controllers/miners.js
+++ app/scripts/controllers/miners.js
@@ -93,7 +93,7 @@ angular.module('atlasApp')
       });
     };
 
-    this.redeploy = function($event) {
+    this.redeploy_all = function($event) {
       var confirm = $mdDialog.confirm()
         .title('Do you want to re-deploy all the miners?')
         .textContent('Re-deploying the miners takes several minutes and will delete all your hosts and miners.')
@@ -119,6 +119,66 @@ angular.module('atlasApp')
           $mdToast.showSimple('Re-deploying started... It will take several minutes');
           $state.reload();
         }, 2000);
+      });
+    };
+
+    this.redeploy = function($event, miner) {
+      var confirm = $mdDialog.confirm()
+        .title('Do you want to re-deploy the miner?')
+        .textContent('Re-deploying the miner takes several minutes and will delete the host and the miner.')
+        .ariaLabel('Re-deploy')
+        .targetEvent($event)
+        .ok('Re-deploy')
+        .cancel('Cancel');
+
+      $mdDialog.show(confirm).then(function() {
+        minersService.remove({
+          id: miner.id
+        });
+
+        hostsService.remove({
+          id: miner.host_id
+        });
+
+        $mdToast.showSimple('Re-deploying started... It will take several minutes');
+
+        setTimeout(function() {
+          if ($state.current.name === 'miners') {
+            $state.reload();
+          } else {
+            $state.go('miners');
+          }
+        }, 2000);
+      });
+    };
+
+    this.remove = function($event, miner) {
+      var confirm = $mdDialog.confirm()
+        .title('Do you want to remove the miner?')
+        .textContent('The hosts will not be deleted. If you want to stop using the host, you need to delete it as well.')
+        .ariaLabel('Remove')
+        .targetEvent($event)
+        .ok('Remove')
+        .cancel('Cancel');
+
+      $mdDialog.show(confirm).then(function() {
+        minersService.remove({
+          id: miner.id
+        }).$promise.then(function() {
+          if ($state.current.name === 'miners') {
+            $state.reload();
+          } else {
+            $state.go('miners');
+          }
+
+          hostsService.update({
+              id: miner.host_id
+            }, {
+              deployed: '0'
+            }).$promise
+            .then(console.log)
+            .catch(console.error);
+        });
       });
     };
   });
